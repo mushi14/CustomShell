e9df431b2fb83a1438900ca405a3353e09f91513
smhassan <smhassan@dons.usfca.edu>
2019-03-19 23:58:30 +0000

    organizing header files

--- built_in.h
+++ built_in.h
@@ -5,4 +5,7 @@
 char NEW_LINE[BUF];
 bool COMMENTS;
 
+int is_file(const char *path);
+char *path_converter(char *path, char *name);
+void change_directory(char *target, bool path);
 void comment_check(char *line);

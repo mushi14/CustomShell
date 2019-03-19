6f149d115533aba9ed41de29a75ad5d73f720f8f
smhassan <smhassan@dons.usfca.edu>
2019-03-19 23:16:42 +0000

    updating

--- built_in.c
+++ built_in.c
@@ -6,7 +6,10 @@
 #include <string.h>
 
 void comment_check(char *line) {
+	COMMENTS = false;
+	memset(NEW_LINE, 0, BUF);
 	if (strstr(line, "#")) {
+		COMMENTS = true;
 		int end_line = strcspn(line, "#");
 		for (int i = 0; i < end_line; i++) {
 			NEW_LINE[i] = line[i];

11ff8f765074ee6f1d716d435bbec02804aedca7
smhassan <smhassan@dons.usfca.edu>
2019-04-01 23:38:03 +0000

    updating built_in

--- built_in.h
+++ built_in.h
@@ -1,11 +1,14 @@
-#define BUF 100
+#define BUF _POSIX_ARG_MAX
 
 #include <stdbool.h>
+#include <limits.h>
 
 char NEW_LINE[BUF];
 bool COMMENTS;
+char ret[10000];
 
 int is_file(const char *path);
 char *path_converter(char *path, char *name);
-void change_directory(char *target, bool path);
+void change_directory(char *target);
 void comment_check(char *line);
+void set_env(char *name, char*value);

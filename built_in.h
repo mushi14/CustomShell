a7d9349d627f9d1c457cafe5f221a29e2420a8cf
smhassan <smhassan@dons.usfca.edu>
2019-04-03 19:31:03 +0000

    updating code

--- built_in.h
+++ built_in.h
@@ -8,7 +8,6 @@ bool COMMENTS;
 char ret[10000];
 
 int is_file(const char *path);
-char *path_converter(char *path, char *name);
 void change_directory(char *target);
 void comment_check(char *line);
 void set_env(char *name, char*value);

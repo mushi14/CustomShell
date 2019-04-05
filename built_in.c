ce7af8a78d6c78d960fa513c5915365dd8aec9b6
smhassan <smhassan@dons.usfca.edu>
2019-04-05 19:21:22 +0000

    90% done

--- built_in.c
+++ built_in.c
@@ -10,6 +10,11 @@
 #include <unistd.h>
 #include <limits.h>
 
+/**
+ * Checks to see if the given path is a file or not
+ * Param: path - fie to analyze
+ * Return: 0 if it is regular file or 1 if it is a directory
+ */
 int is_file(const char *path) {
 	struct stat path_stat;
 	stat(path, &path_stat);
@@ -17,6 +22,11 @@ int is_file(const char *path) {
 	return S_ISREG(path_stat.st_mode);
 }
 
+/**
+ * Changes the current directory to the given target directory
+ * Param: target - target directory to change the current working 
+ *        directory to
+ */
 void change_directory(char *target) {
 	char cwd[PATH_MAX];
 	memset(cwd, 0, PATH_MAX);
@@ -34,6 +44,10 @@ void change_directory(char *target) {
 	closedir(dir);
 }
 
+/**
+ * Checks to see the given line contains any comments
+ * Param: line - line to check
+ */
 void comment_check(char *line) {
 	COMMENTS = false;
 	memset(NEW_LINE, 0, BUF);
@@ -46,6 +60,11 @@ void comment_check(char *line) {
 	}
 }
 
+/**
+ * Sets up a custom environment variable to the given name and value
+ * Param: name - name for the custom environment variable
+ * Param: value - value for the custom environment variable
+ */
 void set_env(char *name, char *value) {
 	int ret = setenv(name, value, 0);
 	if (ret == -1) {
